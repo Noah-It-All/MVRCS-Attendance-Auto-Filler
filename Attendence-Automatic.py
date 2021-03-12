8fab48d6806a269ab61b09b7c6c42c1c299a5d7f
Noah-It-All <noahunderscore69@gmail.com>
2021-03-12 09:43:11 -0500

    Revert "aaaaa"
    This reverts commit d1ecf67b491f06e0c5fa62f42792532b096d258e.

--- Attendence-Automatic.py
+++ Attendence-Automatic.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from time import sleep
 from datetime import datetime
 import tkinter as tk
+from selenium.common.exceptions.webdriver import NoSuchWindowException
+
 
 now = datetime.now()
 thetxt = open('C:/Users/noahu/Desktop/py-projects/theattender/form_fill.txt')
@@ -37,38 +39,49 @@ im_in_attendence = '//*[@id="form-container"]/div/div/div/div/div[1]/div[3]/div[
 im_available = '//*[@id="form-container"]/div/div/div/div/div[1]/div[3]/div[2]/div[7]/div/div[2]/div/div[2]/div/label/input'
 
 submit_button = '//*[@id="form-container"]/div/div/div/div/div[1]/div[3]/div[3]/div[1]/button'
-
-driver.find_element_by_xpath(date_input).send_keys(ledate) #the date field
-driver.find_element_by_xpath(student_id_Input).send_keys(student_id) 
-driver.find_element_by_xpath(first_name_input).send_keys(first_name)
-driver.find_element_by_xpath(Last_Name_Input).send_keys(Last_Name)
-driver.find_element_by_xpath(drop_down_opener).click() #open drop down
-driver.find_element_by_xpath(grade_clicker).click() #click grade 6
-if lineread[4] == "remote":
-    driver.find_element_by_xpath(remote_learning_clicker).click()
-else:
-    if lineread[4] == "hybrid":
-        driver.find_element_by_xpath(hybrid_learning_clicker).click()
+try:
+    driver.get("https://forms.office.com/Pages/ResponsePage.aspx?id=XYl7mAJPm0i5TUkXBGnu90WQw_c63PZOtGrFHbAgXFNUNFFZMkxJT0FBRlMxTVpMVTBDUU1LWDZCUy4u")
+    driver.find_element_by_xpath(date_input).send_keys(ledate) #the date field
+    driver.find_element_by_xpath(student_id_Input).send_keys(student_id) 
+    driver.find_element_by_xpath(first_name_input).send_keys(first_name)
+    driver.find_element_by_xpath(Last_Name_Input).send_keys(Last_Name)
+    driver.find_element_by_xpath(drop_down_opener).click() #open drop down
+    driver.find_element_by_xpath(grade_clicker).click() #click grade 6
+    if lineread[4] == "remote":
+        driver.find_element_by_xpath(remote_learning_clicker).click()
     else:
-        if lineread[4] == "inperson":
-            driver.find_element_by_xpath(inperson_learning_clicker).click()
+        if lineread[4] == "hybrid":
+            driver.find_element_by_xpath(hybrid_learning_clicker).click()
         else:
-            driver.quit
-            root = tk.Tk()
-            T = tk.Text(root, height=5, width=30)
-            T.pack()
-            T.configure(font=("Arial", 16, "bold"))
-            T.insert(tk.END, "You Haven't chosen a learning type yet!\nThe options for learning types ares\nremote, hybrid or inperson\n")
-            tk.mainloop()
-            driver.quit
-driver.find_element_by_xpath(im_in_attendence).click() #click i am in attendence
-driver.find_element_by_xpath(im_available).click() #click i am available
-driver.find_element_by_xpath(submit_button).click() #submit
-driver.quit()
+            if lineread[4] == "inperson":
+                driver.find_element_by_xpath(inperson_learning_clicker).click()
+            else:
+                driver.quit
+                root = tk.Tk()
+                T = tk.Text(root, height=5, width=30)
+                T.pack()
+                T.configure(font=("Arial", 16, "bold"))
+                T.insert(tk.END, "You Haven't chosen a learning type yet!\nThe options for learning types ares\nremote, hybrid or inperson\n")
+                tk.mainloop()
+                driver.quit
+    driver.find_element_by_xpath(im_in_attendence).click() #click i am in attendence
+    driver.find_element_by_xpath(im_available).click() #click i am available
+    driver.find_element_by_xpath(submit_button).click() #submit
+    driver.quit()
+    root = tk.Tk()
+    T = tk.Text(root, height=15, width=30)
+    T.pack()
+    T.insert(tk.END, "made by noah henok")
+    tk.mainloop()
 
-root = tk.Tk()
-T = tk.Text(root, height=15, width=30)
-T.pack()
-T.insert(tk.END, "made by noah henok")
-tk.mainloop()
+except: NoSuchWindowException
+
+else:
+    root = tk.Tk()
+    T = tk.Text(root, height=5, width=30)
+    T.pack()
+    T.configure(font=("Arial", 16, "bold"))
+    T.insert(tk.END, "You closed the window early!\n(Dont do that)\n")
+    tk.mainloop()
+
 
